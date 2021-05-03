fa9e6446aeed8ec1db119ff9fc92153025d01b0c
Charles Averill <charlesaverill20@gmail.com>
2021-05-02 21:12:40 -0500

    Add UnityScripts

---
+++ UnityScripts/js/main.js
@@ -0,0 +1,11 @@
+function copyText(id){
+	var toCopy = document.querySelectorAll(id)[0].outerText;
+
+	const el = document.createElement('textarea');
+	el.value = toCopy;
+	document.body.appendChild(el);
+	el.select();
+
+	document.execCommand('copy');
+	document.body.removeChild(el);
+}
